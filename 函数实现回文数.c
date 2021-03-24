422526b44809c438c58cfd632fa07c407c64acfc
veritas0518 <76930817+veritas0518@users.noreply.github.com>
2021-03-24 11:05:37 +0800

    Palindrome number
    a functional scheme to solve the classical Palindrome number

---
+++ 函数实现回文数.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//void is_reverse(int a) {
+//	if ((a % 10 == a / 10000) && (a /10 % 10 == a / 1000 % 10)) 
+//	{
+//		printf("这是一个回文数");
+//	}
+//	else
+//	{
+//		printf("这不是一个回文数");
+//	}
+//}
+//int main()
+//{
+//	int a;
+//	scanf_s("%d", &a);
+//	is_reverse(a);
+//	//printf("这是一个回文数");
+//	//printf("这不是一个回文数");
+//}
