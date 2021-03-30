70d6bf7edd0d2bf5f8a018e68b87eadb3d9c0bf8
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-03-30 10:59:00 +0800

    I'll never retreat,thought I'm faltering
    do you know that the 'b' in debt is muse

---
+++ 使num每调用一次就会增加1.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//void Add(int* p) {
+//	(*p)++;
+//}
+//int main() {
+//	int num = 0;
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	return 0;
+//}
